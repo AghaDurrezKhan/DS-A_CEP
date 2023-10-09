0a29ef0a91c59fb5f297a3319ba49fc5bedf5a13
Agha Durrez Khan <aghadurrezkhan75@gmail.com>
2023-10-09 23:09:49 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
